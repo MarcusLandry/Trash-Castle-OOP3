7da334cc570f49e29507600bb79e8cda065a9cf9
caleblandry1 <caleb.landry1@dcmail.ca>
2025-04-07 12:25:23 -0400

    added logic for the deck handling script

---
+++ Assets/Scripts/Monobehavior/DeckManager.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckManager : MonoBehaviour
+{
+    public static DeckManager Instance { get; private set; }
+
+    private Deck deck;
+    public Transform drawPileAnchor; // Optional: assign in inspector if you want drawn cards to spawn visually here
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        deck = new Deck();
+    }
+
+    public Card DrawCard()
+    {
+        Card drawn = deck.Draw();
+        if (drawn != null)
+        {
+            Debug.Log("Drew card: " + drawn);
+            // Optionally instantiate a card UI prefab here
+        }
+        else
+        {
+            Debug.LogWarning("Deck is empty!");
+        }
+        return drawn;
+    }
+
+    public void ReturnCard(Card card)
+    {
+        deck.ReturnToDeck(card);
+        deck.Shuffle();
+    }
+}
